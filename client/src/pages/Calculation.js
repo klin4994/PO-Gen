99b0018cd49bcfdee935601788ba02821102deb0
klin4994 <kevlin129@hotmail.com>
2021-05-01 01:21:40 +1000

    Adding Nav-bar component, and set up states in the calculation pg

--- client/src/pages/Calculation.js
+++ client/src/pages/Calculation.js
@@ -1,6 +1,16 @@
 import React, {useState, useEffect} from "react";
 
 function Calculation () {
+        // Overage 5%
+        const overage = 1.05;
+        // Set products state
+        const [products, setProducts] = useState([])
+        // Set product qty state
+        const [quantity, setQuantity] = useState(1)
+        // Set state for currently selected product key 
+        const [currentProductKey, setcurrentProductKey] = useState("")
+        // Set state for currently selected product
+        const [currentProduct, setCurrentProduct] = useState([])
     return
 }
 
