d23311f433e7fe94ec9394460000ec2f3e4df08d
klin4994 <kevlin129@hotmail.com>
2021-05-06 21:29:58 +1000

    Set up form component for login

---
+++ client/src/components/LoginForm/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import './index.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+export default function  ({onFinish, onFinishFailed, ...props}) {
+    const layout = {
+        labelCol: {
+          span: 8,
+        },
+        wrapperCol: {
+          span: 16,
+        },
+        
+      };
+      const tailLayout = {
+        wrapperCol: {
+          offset: 8,
+          span: 16,
+        },
+      };
+    return (
+        <> 
+        <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            style={{border:"solid #f1f1f1", padding:"3em", minWidth:"30%", marginTop: "3em"}}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Username!',
+                },
+              ]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Password!',
+                },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+            </Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+            </Button>
+            </Form>
+    </>
+    )
+}
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const passport = require("./config/passport");
+const passport = require("./config/passport");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
