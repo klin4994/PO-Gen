c65c8005ba3a87c5f6db14d1a7d894e8b45c3edd
klin4994 <kevlin129@hotmail.com>
2021-05-24 20:13:49 +1000

    Heroku

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
   useEffect(() => {
     API.userLoggedIn().then(response => {
       // setIsAuthenticated(response.data.isAuthenticated)
-      setIsAuthenticated(true)
+      setIsAuthenticated(response.data.isAuthenticated)
     })
   }, []);
 
