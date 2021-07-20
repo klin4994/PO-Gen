a00ae8277336d42cecc5178f0b453c86b6e42006
kvlin <kevlin129@hotmail.com>
2021-07-21 00:05:23 +1000

    Footer stays at the bottom style adjustment

--- client/src/pages/AddProduct.js
+++ client/src/pages/AddProduct.js
@@ -70,7 +70,7 @@ export default function AddProduct () {
 
   return (
 
-    <Layout style={{ minHeight: '93vh', maxWidth: '100vh'}}>
+    <Layout style={{ minHeight: '93vh', maxWidth: '100vw'}}>
       <Content style={{ marginTop: '3em' }}>
         <Form.Provider>
           <Form {...layout} name='new-product-form' onFinish={onFinish} autoComplete='off' validateMessages={validateMessages}>
